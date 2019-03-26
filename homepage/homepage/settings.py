b9c86afd8e67ae547f805646a715b640d5a558a7
Traabant <david.siba@gmail.com>
2019-03-26 14:41:57 +0000

    get rid of eroor on pythonanywhere

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -132,6 +132,6 @@ LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
