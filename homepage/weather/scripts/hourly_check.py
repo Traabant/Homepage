f89ffbb25b401204815388999fec04c71034b5b3
siba <david.siba@gmail.com>
2019-04-29 13:56:23 +0200

    fixxed timezone error

--- homepage/weather/scripts/hourly_check.py
+++ homepage/weather/scripts/hourly_check.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import sqlite3
-import datetime
+from datetime import datetime, timedelta
 from sqlite3 import Error
 
 
@@ -23,7 +23,7 @@ def create_connection_to_db(db_file):
 def get_pollution():
     fileDir = '/home/Traabant/Homepage/Homepage/homepage'
     db_file_name = fileDir + '/db.sqlite3'
-    # db_file_name = 'D:\SIBA\Scripty\Homepage\homepage\db.sqlite3'
+    db_file_name = 'D:\SIBA\Scripty\Homepage\homepage\db.sqlite3'
     conection = create_connection_to_db(db_file_name)
     cursor = conection.cursor()
 
@@ -32,12 +32,13 @@ def get_pollution():
     air_pollution_data = json.loads(response.text)
     index_ostrava_portuba = air_pollution_data['States'][0]['Regions'][13]['Stations'][15]['Ix']
     date_pullution = air_pollution_data['States'][0]['DateFromUTC']
-    date_pullution = datetime.datetime.strptime(date_pullution, '%Y-%m-%d %H:%M:%S.%f %Z')
+    date_pullution = datetime.strptime(date_pullution, '%Y-%m-%d %H:%M:%S.%f %Z')
+    date_pullution = date_pullution + timedelta(hours=2)
     date_pullution = date_pullution.strftime('%Y-%m-%d %H:%M:%S')
     string_to_execute = "INSERT INTO weather_pollution(datetime, pollution_index) VALUES('%s', '%d')" % (date_pullution, index_ostrava_portuba)
     cursor.execute(string_to_execute)
     conection.commit()
-    print(f'Aktualni index {index_ostrava_portuba} z {date_pullution} stazeno {datetime.datetime.now()}')
+    print(f'Aktualni index {index_ostrava_portuba} z {date_pullution} stazeno {datetime.now()}')
 
 get_pollution()
 
