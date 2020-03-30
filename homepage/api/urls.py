cd8f930cbee2bba6d020d82592470b896610fd99
David Siba <david.siba@gmail.com>
2020-03-30 11:36:18 +0200

    created DB table and API point to save Temps from home sensor

--- homepage/api/urls.py
+++ homepage/api/urls.py
@@ -5,6 +5,7 @@ weatherpatterns = [
     path('today', views.todayTemps, name='todayTemps'),
     path('tomorow', views.tomorowTemps, name='tomorowTemps'),
     path('pollution', views.pollution, name='pollution'),
+    path('save-home-temps', views.save_temps, name="save-home-temps")
 ] 
 
 githubPatterns = [
@@ -16,6 +17,5 @@ urlpatterns = [
     path('get-images', views.radarImageNames, name='radarImageNames'),
     path('weather/', include(weatherpatterns)),
     path('github/', include(githubPatterns)),
-
 ]
 
