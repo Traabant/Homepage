48ffe14c5fe4323c7afa0fef21d9348393b6d426
David Siba <david.siba@gmail.com>
2020-04-22 11:53:39 +0200

    Created Hello word :)

---
+++ fe/src/Helloword.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Helloword(){
+    return(
+        <div>
+            <h1>Hello word!</h1>
+        </div>
+    );
+}
+
+export default Helloword;
--- fe/src/index.js
+++ fe/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Helloword from './Helloword';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Helloword />
   </React.StrictMode>,
   document.getElementById('root')
 );
